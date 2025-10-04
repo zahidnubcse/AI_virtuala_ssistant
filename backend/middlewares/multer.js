6446cb92fe992ebba1231c8b7d558a4a60481600
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2025-10-05 01:03:04 +0600

    problem solved

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -2,10 +2,10 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public"); // upload folder
+    cb(null, "./public");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname); // keep original name
+    cb(null, file.originalname);
   },
 });
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,1 +1,11 @@
-C
+import express from "express";
+import { getCurrentUser, updateAssistant } from "../controllers/userController.js";
+import isAuth from "../middlewares/isAuth.js";
+import upload from "../middlewares/multer.js";
+
+const userRouter = express.Router();
+
+userRouter.get("/current", isAuth, getCurrentUser);
+userRouter.post("/update", isAuth, upload.single("assistantImage"), updateAssistant);
+
+export default userRouter;
